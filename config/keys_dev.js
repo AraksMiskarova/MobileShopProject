77b544d21080dded35a9d1134ec1e09ad133a33d
Araks Miskarova <araksmiskarova@gmail.com>
2023-06-09 12:58:46 +0200

    token refresh

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI:
-        'mongodb+srv://mobileShop:fdQy4FBNU5LG@mobilemagazine.ooncqrv.mongodb.net/test',
+    'mongodb+srv://mobileShop:fdQy4FBNU5LG@mobilemagazine.ooncqrv.mongodb.net/test',
     secretOrKey: 'random very very secret string',
 };
