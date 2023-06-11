5d98a8224ec8efa489cffc7424717edcd8a72812
Araks Miskarova <araksmiskarova@gmail.com>
2023-06-11 12:33:00 +0200

    Login refresh token

--- client/src/redux/slices/customer.js
+++ client/src/redux/slices/customer.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+import api from '../../helpers/toolkit/api';
 import { createAsyncReducer } from '../../helpers/toolkit/extraReducers';
 import { fetchData } from '../../helpers/toolkit/fetches';
 
@@ -10,8 +11,11 @@ export const fetchCustomerData = createAsyncThunk(
   },
 );
 
-export const getCustomers = createAsyncThunk('customer', async () => {
-  return fetchData(`/api/customers`, 'get');
+export const getCustomers = createAsyncThunk('customers', async () => {
+  // eslint-disable-next-line no-return-await
+  return await api.get('api/customers').then(response => {
+    return response.data;
+  });
 });
 
 const initialState = {
