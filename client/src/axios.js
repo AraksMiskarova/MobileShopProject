f95b3f31471b292c1852bcf40b2d618e62997009
Araks Miskarova <araksmiskarova@gmail.com>
2023-07-11 21:27:06 +0200

    update for baseUrl

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '',
+  baseURL: 'https://mobileshop-api.onrender.com/',
 });
 
 instance.interceptors.request.use(config => {
