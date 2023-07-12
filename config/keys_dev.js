4db96138a19f255aa15aa103476938613e90ab0d
Araks Miskarova <araksmiskarova@gmail.com>
2023-07-12 14:26:48 +0200

    update

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    // "mongodb+srv://mobileShop:fdQy4FBNU5LG@mobilemagazine.ooncqrv.mongodb.net/test",
-    "mongodb+srv://admin:fwftrtr7u36U98qC@mobilemagazine.ooncqrv.mongodb.net/test",
-  secretOrKey: "random very very secret string",
-};
